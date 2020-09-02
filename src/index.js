15bcc55868d831d75fd8b5043dac40d0b03cb1ea
Giovanni Solano <giovannisolano211@gmail.com>
2020-09-01 21:18:02 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+// const tarea = new Todo('Aprender JS!!');
+
+// todoList.nuevoTodo(tarea);
+
+// console.log(todoList);
+
+// crearTodoHtml(tarea);
+
+// todoList.todos.forEach(todo => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml);
